01b41450a262d3ecad9bf1c4b574da85fe58ee7e
Kimi Phan <phanlkim@comcast.net>
2018-10-29 11:44:49 -0700

    working on Lab7

---
+++ Lab7/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <ext2fs/ext2_fs.h>
+#include <stdint.h>
+#include <string.h>
+#include "types.h"
+#include "utils.h"
+#include "commands.h"
+
+#define MAX 256
+
+int main(int argc, char *argv[])
+{
+  char *dev = NULL, line[MAX];
+  PROC *processes = NULL, *running = NULL;
+  MINODE *minode = NULL, *root = NULL;
+  struct mntTable *mtable = NULL;
+  int fd, numArgs, cmdIndex, count, size;
+  char **myargv = NULL;
+  char *lineCopy = NULL, *buf = NULL, *name = NULL;
+
+  if (argc < 2)
+  {
+    printf("Usage: %s [disk filename]\n", argv[0]);
+    exit(1);
+  }
+
+  dev = argv[1];
+
+  
+}
---
+++ Lab7/utils.h
@@ -0,0 +1,14 @@
+#ifndef __UTIL_H__
+#define __UTIL_H__
+
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "types.h"
+
+#define BLOCK_SIZE 1024
