c6e0d578f400c1c2d4384b8fb6396db7a3b7fc03
Kimi Phan <phanlkim@comcast.net>
2018-10-30 13:55:53 -0700

    working on the wait.c file

--- Lab6/wait.c
+++ Lab6/wait.c
@@ -1,9 +1,31 @@
 int ksleep(int event)
 {
+  printf("proc %d ksleep on %x\n", running->pid, event);
+  running->event = event;
+  running->status = SLEEP;
+  enqueue(&sleepList, running);
+  printf("sleepList = ");
+  printQ(sleepList);
+  tswitch();
 }
 
 int kwakeup(int event)
 {
+  PROC *p, *tmp = 0;
+  while((p = dequeue(&sleepList)) != 0)
+  {
+    if (p->event == event)
+    {
+      printf("kwakeup %d\n", p->pid);
+      p->status = READY;
+      enqueue(&readyQueue, p);
+    }
+    else
+    {
+      enqueue(&tmp, p);
+    }
+  }
+  sleepList = tmp;
 }
 
 int kexit(int value)
