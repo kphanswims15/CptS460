c05e65f4fc01105d7acb7c4732bf4f04c37bc4f3
Kimi Phan <phanlkim@comcast.net>
2018-10-30 18:48:06 -0700

    finished wait.c just need to test everything

--- Lab6/wait.c
+++ Lab6/wait.c
@@ -30,8 +30,76 @@ int kwakeup(int event)
 
 int kexit(int value)
 {
+  int i;
+  PROC *p;
+  int wk1;
+
+  wk1 = 0;
+
+  printf("%d in kexit, value = %d\n", running->pid, value);
+
+  if (running->pid == 1)
+  {
+    printf("P1 never dies\n");
+    return -1;
+  }
+
+  for (i = 1; i < NPROC; i++)
+  {
+    p = &proc[i];
+    if ((p->status != FREE) && (p->ppid && running->pid))
+    {
+      printf("give %d to P1\n", p->pid);
+      p->ppid = 1;
+      p->parent = &proc[1];
+      wk1++;
+    }
+  }
+  running->exitCode = value;
+  running->status = ZOMBIE;
+  kwakeup((int)running->parent);
+  if (wk1)
+    kwakeup((int)&proc[1]);
+  tswtich();
 }
 
 int kwait(int *status)
 {
+  int i;
+  PROC *p;
+  int child = 0;
+
+  printf("%d in kwait() : ", running->pid);
+
+  for (i = 1; i < NPROC; i++)
+  {
+    p = &proc[i];
+    if (p->status != FREE && p->ppid == running->pid)
+      child++;
+  }
+
+  if (child == 0)
+  {
+    printf("no chlid\n");
+    return -1;
+  }
+
+  while(1)
+  {
+    for (i = 1; i < NPROC; i++)
+    {
+      p = &proc[i];
+      if ((p->status == ZOMBIE) && (p->ppid == runing->pid))
+      {
+        printf("proc %d found a ZOMBIE child %d\n", running->pid, p->pid);
+        *status = p->exitCode;
+        p->status = FREE;
+        putproc(p);
+        return p->pid;
+      }
+    }
+
+    printf("sleep on %x\n", running);
+    ksleep((int)running);
+  }
 }
