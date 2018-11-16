ef82584119b3783bf60649f80ee46192cd775d76
Kimi Phan <phanlkim@comcast.net>
2018-11-15 21:39:17 -0800

    commiting things

--- Lab6/USER/init.c
+++ Lab6/USER/init.c
@@ -2,5 +2,22 @@
 
 int main(int argc, char *argv[])
 {
-  // as INIT
+  int sh, pid, status;
+
+  sh = fork();
+  if (sh)     // P1 runs in a while(1) loop
+  {
+    while(1)
+    {
+      pid = wait(&status); // wait for ANY child to die
+      if (pid == sh)      // if sh died, fork another one
+      {
+        sh = fork();
+        continue;
+      }
+      printf("P1: I just buried an orphan %d\n", pid);
+    }
+  }
+  else
+    exec("sh");   // child of P1 runs sh
 }
--- Lab6/USER/sh.c
+++ Lab6/USER/sh.c
@@ -2,5 +2,24 @@
 
 int main(int argc, char *argv[])
 {
-  // as sh process
+  int pid, status;
+  char cmdline[128];
+
+  while(1)
+  {
+    // display executable commands in /bin directory
+    // prompt for a command line cmdline = "cmd a1 a2 .... an"
+
+
+    if(!strcmp(cmd, "exit"))
+    {
+      exit(0);
+    }
+    // fork a child process to execute the cmd line
+    pid = fork();
+    if(pid) // parent sh waits for child to die
+      pid = wait(&status);
+    else // child exec cmdline
+      exec(cmdline);    // exec("cmd a1 a2 .... an")
+  }
 }
