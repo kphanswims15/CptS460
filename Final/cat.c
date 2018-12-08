757dd6119ed842fae6c16b22df4b35ff0bb5c43a
Kimi Phan <phanlkim@comcast.net>
2018-12-07 17:50:08 -0800

    I think cat is finished

--- Final/cat.c
+++ Final/cat.c
@@ -5,12 +5,40 @@ main(int argc, char *argv[])
   int in, out, n;
   char buf[1024], tty[128];
 
-  in = open(argv[1], O_RDONLY);
-  out = 1;
-
-  while(n = read(in, buf, 1024))
+  if (argc == 1)
   {
-    buf[n] = 0;
-    prints(buf);
+    in = 0;
+    out = 1;
   }
+  else
+  {
+    // catting a file
+    in = open(argv[1], O_RDONLY);
+    out = 1;
+  }
+
+  // loop to read the file byte from byte 
+  while(1)
+  {
+    n = read(in, buf, 1);
+    if (n < 1)
+    {
+      break;
+    }
+
+    if (buf[0] == '\n')
+    {
+      prints("\n\r");
+    }
+    // if the user enters the enter key
+    else if (buf[0] == 13)
+    {
+      prints("\n\r");
+    }
+    else
+    {
+      write(out, buf, 1);
+    }
+  }
+  close(in);
 }
